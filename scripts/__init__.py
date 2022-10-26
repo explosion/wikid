b9adc108cd7216e44169381b46df6b4c3785278f
Raphael Mitsch <r.mitsch@outlook.com>
2022-10-26 14:03:55 +0200

    Refactor public API.

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,2 +1,19 @@
-from .wiki import *
-from .schemas import *
+from .wiki import (
+    schemas,
+    load_entities,
+    establish_db_connection,
+    extract_demo_dump,
+    load_alias_entity_prior_probabilities,
+    parse,
+    namespaces,
+)
+
+__all__ = [
+    "schemas",
+    "load_entities",
+    "establish_db_connection",
+    "extract_demo_dump",
+    "load_alias_entity_prior_probabilities",
+    "parse",
+    "namespaces",
+]
