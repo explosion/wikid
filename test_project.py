cd3c2415726787c1a7f7b199d45fed815406a6e4
Raphael Mitsch <r.mitsch@outlook.com>
2022-11-21 13:23:05 +0100

    Remove Windows test skip marker.

--- test_project.py
+++ test_project.py
@@ -1,15 +1,9 @@
 """ Testing all project steps. """
-import pytest
 from pathlib import Path
-import sys
 from spacy.cli.project.run import project_run
 from spacy.cli.project.assets import project_assets
 
 
-@pytest.mark.skipif(
-    sys.platform == "win32",
-    reason="Skipping on Windows (for now) due to platform-specific scripts.",
-)
 def test_project():
     root = Path(__file__).parent
     project_assets(root)
