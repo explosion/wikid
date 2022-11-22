3adc84e89af4afefdb09ace5098075b6476d9bba
Raphael Mitsch <r.mitsch@outlook.com>
2022-11-22 08:52:31 +0100

    Update scripts/kb.py
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>

--- scripts/kb.py
+++ scripts/kb.py
@@ -51,7 +51,7 @@ class WikiKB(KnowledgeBase):
         top_k_aliases (int): Top k aliases matches to consider. An alias may be associated with more than one entity, so
             this parameter does _not_ necessarily correspond to the the maximum number of identified candidates. For
             that use top_k_alias_entity.
-        top_k_alias_entity (int): Top k entities to consider in list of alias matches. Equals maximum number of
+        top_k_entities_alias (int): Top k entities to consider in list of alias matches. Equals maximum number of
             candidate entities found via alias search.
         top_k_entities_fts (int): Top k of full-text search matches to consider. Equals maximum number of candidate entities
             found via full-text search.
