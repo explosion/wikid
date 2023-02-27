6e3f309aa3453fb7e9d789f91c77d128e9bdb0ab
Raphael Mitsch <r.mitsch@outlook.com>
2023-02-27 16:57:51 +0100

    Refactor `scripts` directory (#14)
    * Update KB usage (#10)
    
    * Update KB usage.
    
    * Add conditional import for default KB.
    
    * Use `pysqlite3`, if available (#9)
    
    * Use pysqlite3, if available.
    
    * Introduce compat.py for pysqlite3/sqlite3 imports.
    
    * Fix import error handling for default KB. (#11)
    
    * Refactor scripts to src dir. Rewrite __init__.py.
    
    * Fix path to CLI scripts.
    
    * Update step name in Azure pipeline.
    
    * Remove comment in requirements.txt.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,17 @@
-from .scripts import *
+from .src import (
+    schemas,
+    load_entities,
+    establish_db_connection,
+    load_alias_entity_prior_probabilities,
+    parse,
+    namespaces,
+)
+
+__all__ = [
+    "schemas",
+    "load_entities",
+    "establish_db_connection",
+    "load_alias_entity_prior_probabilities",
+    "parse",
+    "namespaces",
+]
